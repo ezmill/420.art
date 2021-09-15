5e2ffecc29b56315d4aa235042dc6f93ebdce18c
Steve Klebanoff <steve.klebanoff@gmail.com>
2021-09-14 20:08:06 -0700

    demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,46 @@
 import Head from "next/head";
 import React, { useEffect, useState } from "react";
-import { GifsResult, GiphyFetch } from '@giphy/js-fetch-api'
+import { GifsResult, GiphyFetch } from "@giphy/js-fetch-api";
 
-
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
 
 const Index = (props: any) => {
-  const [gifs, setGifs] = useState<GifsResult| undefined>(undefined);
-  
+  const [gifs, setGifs] = useState<GifsResult | undefined>(undefined);
+  const [curIndex, setCurIndex] = useState<number>(0);
+
   const apiKey = process.env.NEXT_PUBLIC_GIPHY_API_KEY;
   if (!apiKey) {
-    throw new Error('no api key');
+    throw new Error("no api key");
   }
   const gf = new GiphyFetch(apiKey);
   useEffect(() => {
-    gf.search('weed', {
-      limit: 50
+    gf.search("cannabis", {
+      limit: 50,
     }).then((x) => {
-      console.log('got gifs', x);
       setGifs(x);
-    })
+    });
   });
-  
-  const res = gifs ? <div>{gifs.data.map(x => <div key={x.id}><img src={x.images.original.url}/></div>)}</div> : <div>no gifs</div>;
-  
+
+  const setRandom = () => {
+    const randomNumber = getRandomInt(49);
+    setCurIndex(randomNumber);
+  };
+
+  useEffect(() => {
+    const interval = window.setInterval(setRandom, 5000);
+    setRandom();
+
+    return () => {
+      window.clearInterval(interval);
+    };
+  });
+
+  let mainStyle: React.CSSProperties = {};
+  if (gifs) {
+    mainStyle.backgroundImage = `url("${gifs.data[curIndex].images.original.url}")`;
+  }
   return (
     <div>
       <Head>
@@ -32,11 +50,18 @@ const Index = (props: any) => {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
       </Head>
-          <main>
-            <div className="text-2xl">
-            {res}..
-            </div>
-          </main>
+      <main
+        style={mainStyle}
+        className="flex h-screen w-screen bg-gradient-to-b from-gray-100 to-gray-300"
+      >
+        <iframe
+          src="https://www.solvency.art/view/420?fullscreen=true&embed=true&display=iframe&size=large"
+          frameBorder="0"
+          loading="lazy"
+          allow="'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          className="h-1/2 w-1/2 m-auto"
+        ></iframe>
+      </main>
     </div>
   );
 };
